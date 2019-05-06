c0066bc377ef003f066850c4d42e7918cea570b2
TL <nbotapp1@gmail.com>
2019-05-06 18:14:05 -0400

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,17 @@
-# Code your solution here!
+require "pry"
+
+def run_guessing_game
+	response = ""
+	puts "Guess a number between 1 and 6."
+	response = gets.chomp
+	random_int = rand(1..6)
+	if response == "exit"
+		puts "Goodbye!"
+		exit
+	elsif Integer(response) == random_int
+		puts "You guessed the correct number!"
+	else
+		puts "The computer guessed #{random_int}."
+	end
+
+end
